4365d9640db69848d186ac471ffd66e822cd5a3e
ZhouxiangZhan <3150102092@zju.edu.cn>
2017-04-03 20:05:32 +0800

    add Itype and Jtype

--- assemble.cpp
+++ assemble.cpp
@@ -2,13 +2,15 @@
 
 Assemble::Assemble():code("")
 {
-    QString a = QString("zero, ,v0,v1,a0,a1,a2,a3,"\
-                    "t0,t1,t2,t3,t4,t5,t6,t7,"\
-                    "s0,s1,s2,s3,s4,s5,s6,s7,"\
-                    "t8,t9, , ,gp,sp,fp,ra");
-    Reglist = a.split(",");
-    QString b = QString("add,addu,sub,subu,and,or,xor,nor,sll,srl,sra,slt,sltu,jr,jalr");
-    Rtypelist = b.split(",");
+    Reglist = QString("zero, ,v0,v1,a0,a1,a2,a3,"\
+                      "t0,t1,t2,t3,t4,t5,t6,t7,"\
+                      "s0,s1,s2,s3,s4,s5,s6,s7,"\
+                      "t8,t9, , ,gp,sp,fp,ra").split(",");
+    Rtypelist = QString("add,addu,sub,subu,and,or,xor,nor,"\
+                        "sll,srl,sra,slt,sltu,jr,jalr").split(",");
+    Itypelist = QString("addi,addiu,slti,sltiu,andi,ori,xori,lui,"\
+                        "lw,lb,lbu,lh,lhu,sw,sh,sb,beq,bne,blez,bgtz,bltz,bgez").split(",");
+    Jtypelist = QString("j,jal").split(",");
 }
 
 void Assemble::Read(const QString &content)
@@ -39,9 +41,12 @@ void Assemble::inst_handle()
     for(auto i = code.begin(); i != code.end(); ++i)
     {
         QStringList line_split = i->split(QRegExp("\\s+|\\s*[,]\\s*|\\s*[;]"));
-        qDebug() << line_split;
         if(Rtypelist.contains(line_split[0]) == true)
             Rtype_inst(line_split);
+        else if(Itypelist.contains(line_split[0]) == true)
+            Itype_inst(line_split, i - code.begin());
+        else if(Jtypelist.contains(line_split[0]) == true)
+            Jtype_inst(line_split, i - code.begin());
         //
     }
 }
@@ -67,19 +72,10 @@ void Assemble::Rtype_inst(const QStringList &inst)
         }
         else if(inst[i][0] == '$' || inst[i][0] == 'r')
         {
-            QString temp = "";
-            if(inst[i] == "$zero")
-                temp = "zero";
-            else
-                temp = inst[i].mid(1,2);
+            QString temp = inst[i].mid(1);
             if(Reglist.contains(temp) == true || QRegExp("[1-2]?[0-9]|[30]|[31]").indexIn(temp) != -1)
             {
-                auto j = Reglist.begin();
-                for(; j != Reglist.end(); ++j)
-                    if(*j == temp)
-                        break;
-                int reg = j - Reglist.begin();
-
+                int reg = get_reg(temp);
                 switch (i)
                 {
                     {
@@ -106,16 +102,8 @@ void Assemble::Rtype_inst(const QStringList &inst)
             }
             //
         }
-
-
     }
-    auto i = Rtypelist.begin();
-    for(; i != Rtypelist.end(); ++i)
-    {
-        if(*i == inst[0])
-            break;
-    }
-    int mark = i - Rtypelist.begin();
+    int mark = get_inst(inst[0], "R");
     switch (mark)
     {
     case ADD:
@@ -170,11 +158,278 @@ void Assemble::Rtype_inst(const QStringList &inst)
     qDebug() << bi_inst;
 }
 
-void Assemble::complete_inst(QString &inst, const int &num, int func_num, int base)
+void Assemble::Itype_inst(const QStringList &inst, const int &inst_num)
+{
+    QString bi_inst = "";
+    QString opcode = "";
+    QString rs = "", rt = "";
+    QString imm = "";
+    if(Itypelist.mid(ADDI, 7).contains(inst[0]) == true)
+    {
+        int op = get_inst(inst[0], "I");
+        complete_inst(opcode, 6, op + 8, 2);
+        for(int i = 1; i < 4; ++i)
+        {
+            int reg = get_reg(inst[i].mid(1));
+            switch (i)
+            {
+            case 1:
+                complete_inst(rt, 5, reg, 2);
+                break;
+            case 2:
+                complete_inst(rs, 5, reg, 2);
+                break;
+            case 3:
+            {
+                int imm_num = inst[i].toInt();
+                if(Itypelist.mid(ADDI, 4).contains(inst[0]) == true
+                        && imm_num < 0)
+                    complete_inst(imm, 16, qAbs(imm_num), 2, "1");
+                else if(Itypelist.mid(ANDI, 3).contains(inst[0]) == true
+                        || imm_num >= 0)
+                    complete_inst(imm, 16, imm_num, 2);
+                break;
+            }
+            default:
+                break;
+            }
+        }
+    }
+    else if(Itypelist.mid(LUI, 1).contains(inst[0]) == true)
+    {
+        int op = get_inst(inst[0], "I");
+        complete_inst(opcode, 6, op + 8, 2);
+        for(int i = 1; i < 4; ++i)
+        {
+            int reg = get_reg(inst[i].mid(1));
+            complete_inst(rs, 5, 0, 2);
+            switch (i)
+            {
+            case 1:
+                complete_inst(rt, 5, reg, 2);
+                break;
+            case 2:
+            {
+                int imm_num = inst[i].toInt();
+                if(imm_num < 0)
+                    complete_inst(imm, 16, qAbs(imm_num), 2, "1");
+                else
+                    complete_inst(imm, 16, imm_num, 2);
+                break;
+            }
+            default:
+                break;
+            }
+        }
+    }
+    else if(Itypelist.mid(LW, 8).contains(inst[0]) == true)
+    {
+        QString offset = "";
+        int mark = get_inst(inst[0], "I");
+        switch (mark)
+        {
+        case LB:
+            complete_inst(opcode, 6, 32, 2);
+            break;
+        case LBU:
+            complete_inst(opcode, 6, 36, 2);
+            break;
+        case LH:
+            complete_inst(opcode, 6, 33, 2);
+            break;
+        case LHU:
+            complete_inst(opcode, 6, 37, 2);
+            break;
+        case LW:
+            complete_inst(opcode, 6, 35, 2);
+            break;
+        case SB:
+            complete_inst(opcode, 6, 40, 2);
+            break;
+        case SH:
+            complete_inst(opcode, 6, 41, 2);
+            break;
+        case SW:
+            complete_inst(opcode, 6, 43, 2);
+            break;
+        default:
+            break;
+        }
+        for(int i = 1; i <= 2; ++i)
+        {
+            int reg;
+            int num;
+            int index;
+            switch (i) {
+            case 1:
+                reg = get_reg(inst[i].mid(1));
+                complete_inst(rt, 5, reg, 2);
+                break;
+            case 2:
+                if(inst[i].indexOf("(") != -1)
+                {
+                    index = inst[i].indexOf("(");
+                    num = inst[i].mid(0, index).toInt();
+                    if(num < 0)
+                        complete_inst(offset, 16, qAbs(num), 2, "1");
+                    else
+                        complete_inst(offset, 16, num, 2);
+                    if(inst[i][index + 1] == "$" && inst[i].endsWith(")"))
+                    {
+                        int reg = get_reg(inst[i].mid(index + 2).remove(")"));
+                        complete_inst(rs, 5, reg, 2);
+                    }
+                }
+                else
+                {
+
+                }
+            default:
+                break;
+            }
+            imm = offset;
+        }
+    }
+    else if(Itypelist.mid(BEQ, 6).contains(inst[0]) == true)
+    {
+        int op = get_inst(inst[0], "I");
+        if(inst[0] != "bltz" && inst[0] != "bgez")
+            complete_inst(opcode, 6, op - 12, 2);
+        else
+        {
+            complete_inst(opcode, 6, 1, 2);
+        }
+
+        for(int i = 1; i < 4; ++i)
+        {
+            int reg;
+            switch (i)
+            {
+            case 1:
+                reg = get_reg(inst[i].mid(1));
+                complete_inst(rs, 5, reg, 2);
+                break;
+            case 2:
+                reg = get_reg(inst[i].mid(1));
+                if(inst[0] != "bltz" && inst[0] != "bgez" && inst[0] != "bgtz" && inst[0] != "blez")
+                    complete_inst(rt, 5, reg, 2);
+                else
+                {
+                    complete_inst(rt, 5, 0, 2);
+                    int offset;
+                    if(inst[i].contains(QRegExp("^[1-9]?[0-9]*$")) == true)
+                        offset = inst[i].toInt();
+                    else
+                        offset = map[inst[i]] - inst_num - 1;
+                    if(offset < 0)
+                        complete_inst(imm, 16, qAbs(offset), 2, "1");
+                    else
+                        complete_inst(imm, 16, offset, 2);
+                }
+                break;
+            case 3:
+            {
+                if(inst[0] != "bltz" && inst[0] != "bgez" && inst[0] != "bgtz" && inst[0] != "blez")
+                {
+                    int offset;
+                    if(inst[i].contains(QRegExp("^[1-9]?[0-9]*$")) == true)
+                        offset = inst[i].toInt();
+                    else
+                        offset = map[inst[i]] - inst_num - 1;
+                    if(offset < 0)
+                        complete_inst(imm, 16, qAbs(offset), 2, "1");
+                    else
+                        complete_inst(imm, 16, offset, 2);
+                }
+                break;
+            }
+            default:
+                break;
+            }
+        }
+    }
+    bi_inst = opcode + rs + rt + imm;
+    qDebug() << bi_inst;
+}
+
+void Assemble::Jtype_inst(const QStringList &inst)
+{
+    QString bi_inst = "";
+    QString opcode = "";
+    QString imm = "";
+    int op = get_inst(inst[0], "J");
+    complete_inst(opcode, 6, 2 + op, 2);
+    int offset;
+    if(inst[1].contains(QRegExp("^[1-9]?[0-9]*$")) == true)
+        offset = inst[1].toInt();
+    else
+        offset = map[inst[1]];
+    if(offset < 0)
+        complete_inst(imm, 26, qAbs(offset), 2, "1");
+    else
+        complete_inst(imm, 26, offset, 2);
+    bi_inst = opcode + imm;
+    qDebug() << bi_inst;
+}
+
+void Assemble::complete_inst(QString &inst, const int &num, const int &func_num, const int &base,
+                             const QString &fill, const int &isfront)
 {
     inst = QString::number(func_num, base);
+    if(fill == "1")
+    {
+        for(auto i = inst.begin(); i != inst.end(); ++i)
+        {
+            if(*i == "0")
+                inst.replace(i - inst.begin(), 1, '1');
+            else
+                inst.replace(i - inst.begin(), 1, '0');
+        }
+        if(*(inst.end() - 1) == "0")
+            inst.replace(inst.length() - 1, 1, '1');
+        else
+            inst.replace(inst.length() - 1, 1, '0');
+    }
     while(inst.length() < num)
     {
-        inst.push_front('0');
+        if(isfront == 1)
+            inst.push_front(fill);
+        else
+            inst.push_back(fill);
     }
+}
+int Assemble::get_reg(const QString &reg) const
+{
+    auto j = Reglist.begin();
+    for(; j != Reglist.end(); ++j)
+        if(*j == reg)
+            break;
+    return j - Reglist.begin();
+}
+
+int Assemble::get_inst(const QString &inst, const QString &type) const
+{
+    decltype(Rtypelist.begin()) j, begin, end;
+    if(type == "R")
+    {
+        begin = Rtypelist.begin();
+        j = Rtypelist.begin();
+        end = Rtypelist.end();
+    }
+    else if(type == "I")
+    {
+        begin = Itypelist.begin();
+        j = Itypelist.begin();
+        end = Itypelist.end();
+    }
+    else if(type == "J")
+    {
+        begin = Jtypelist.begin();
+        j = Jtypelist.begin();
+        end = Jtypelist.end();
+    }
+    for(; j != end; ++j)
+        if(*j == inst)
+            break;
+    return j - begin;
 }
--- assemble.h
+++ assemble.h
@@ -6,8 +6,10 @@
 #include <QMap>
 #include <QDebug>
 
-QStringList Rtypelist;
 QStringList Reglist;
+QStringList Rtypelist;
+QStringList Itypelist;
+QStringList Jtypelist;
 
 class Assemble
 {
@@ -46,8 +48,45 @@ private:
         JR,
         JALR
     };
+    enum Itype
+    {
+        ADDI,
+        ADDIU,
+        SLTI,
+        SLTIU,
+        ANDI,
+        ORI,
+        XORI,
+        LUI,
+        LW,
+        LB,
+        LBU,
+        LH,
+        LHU,
+        SW,
+        SH,
+        SB,
+        BEQ,
+        BNE,
+        BLEZ,
+        BGTZ,
+        BLTZ,
+        BGEZ
+    };
+
+    enum Jtype
+    {
+        J,
+        Jal
+    };
+
     void Rtype_inst(const QStringList &inst);
-    void complete_inst(QString &inst, const int &num, int func_num, int base);
+    void Itype_inst(const QStringList &inst, const int &inst_num);
+    void Jtype_inst(const QStringList &inst);
+    void complete_inst(QString &inst, const int &num, const int &func_num, const int &base,
+                       const QString &fill = "0", const int &isfront = 1);
+    int get_reg(const QString &reg) const;
+    int get_inst(const QString &inst, const QString &type) const;
 };
 
 #endif // ASSEMBLE_H
